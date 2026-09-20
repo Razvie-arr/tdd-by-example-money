7deb9ed3261fd2431e93cbb0ebc63ed8ca613afa
Razvie-arr <vladikbyastinov@gmail.com>
2026-09-20 23:30:01 +0200

    Chapter 3: Equality For All

--- src/test/kotlin/MoneyTest.kt
+++ src/test/kotlin/MoneyTest.kt
@@ -1,6 +1,7 @@
 import org.example.Dollar
 import org.junit.jupiter.api.Test
 import kotlin.test.assertEquals
+import kotlin.test.assertNotEquals
 
 class MoneyTest {
 
@@ -12,6 +13,12 @@ class MoneyTest {
 
         product = five.times(3)
         assertEquals(15, product.amount)
+    }
+
+    @Test
+    fun `test equality`() {
+        assertEquals(Dollar(5), Dollar(5))
+        assertNotEquals(Dollar(5), Dollar(6))
     }
 
 }
