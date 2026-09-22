5e81771908f96b85c75887f141cde7c0a0cd3ad7
Razvie-arr <vladikbyastinov@gmail.com>
2026-09-22 20:46:28 +0200

    Chapter 15: Mixed Currencies

--- src/main/kotlin/Money.kt
+++ src/main/kotlin/Money.kt
@@ -7,9 +7,9 @@ class Money(val amount: Int, val currency: String) : Expression {
         fun franc(amount: Int): Money = Money(amount, "CHF")
     }
 
-    fun times(multiplier: Int) = Money(amount * multiplier, currency)
+    fun times(multiplier: Int): Expression = Money(amount * multiplier, currency)
 
-    fun plus(addend: Money): Expression = Sum(this, addend)
+    override fun plus(addend: Expression): Expression = Sum(this, addend)
 
     override fun equals(other: Any?): Boolean {
         val money: Money = other as Money
--- src/test/kotlin/MoneyTest.kt
+++ src/test/kotlin/MoneyTest.kt
@@ -74,4 +74,16 @@ class MoneyTest {
         assertEquals(1, Bank().rate("USD", "USD"))
     }
 
+    @Test
+    fun `test mixed addition`() {
+        val fiveBucks: Expression = Money.dollar(5)
+        val tenFrancs: Expression = Money.franc(10)
+
+        val bank = Bank()
+        bank.addRate("CHF", "USD", 2)
+
+        val result = bank.reduce(fiveBucks.plus(tenFrancs), "USD")
+        assertEquals(Money.dollar(10), result)
+    }
+
 }
